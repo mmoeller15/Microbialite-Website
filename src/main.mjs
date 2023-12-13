3916f02c53894081c67fd3f073362d34afce576b
Chandler Carlin <chandlercarlin01@gmail.com>
2023-12-12 22:50:14 -0600

    cat sound bite

--- src/main.mjs
+++ src/main.mjs
@@ -9,19 +9,6 @@ const db = new sqlite3.Database('microbialite.sqlite3', (err) => {
     }
 });
 
-function dbSelect(query, params) {
-    return new Promise((resolve, reject) => {
-        db.all(query, params, (err, rows) => {
-            if (err) {
-                reject(err);
-            }
-            else {
-                resolve(rows);
-            }
-        });
-    });
-}
-
 const dataSql = fs.readFileSync('./sql/ProjectPhase5TableCreation.sql').toString();
 const dataArr = dataSql.split(');');
 
