6c07531d6cc6967b920da36b478b46deeae364b8
Chandler Carlin <chandlercarlin01@gmail.com>
2023-12-14 13:45:08 -0600

    be at ease, my friends

--- app.js
+++ app.js
@@ -15,40 +15,154 @@ const db = new sqlite3.Database('microbialite.sqlite3', (err) => {
     }
 });
 
+function dbSelect(table, form_data) {
+    let query = "SELECT * FROM " + table;
+    let flag = false;
+    for (const key in form_data) {
+        if (form_data[key] != '') {
+            query += " WHERE ";
+            flag = true;
+            break;
+        }
+    }
+
+    if (flag) {
+        for (const key in form_data) {
+            let x = parseFloat(form_data[key]);
+            if (form_data[key] != '') {
+                if (!isNaN(x) && !form_data[key].includes("-")) {
+                    query += key + " = " + form_data[key] + " AND ";
+                } else {
+                    query += key + " = '" + form_data[key] + "' AND ";
+                }
+            }
+        }
+        query = query.slice(0, -5);
+    }
+    query += ";";
+    console.log(query);
+
+    return new Promise ((resolve) => {
+        db.all(query, (err, rows) => {
+            if (err) {
+                console.log(err);
+            } else {
+                resolve(rows);
+            }
+        });
+    });
+}
+
+function dbUpdate(table, form_data) {
+    let id_key = Object.keys(form_data)[0];
+    let id_value = form_data[Object.keys(form_data)[0]];
+    let query = "UPDATE " + table + " SET ";
+    for (const key in form_data) {
+        if (key != id_key) {
+            let x = parseFloat(form_data[key]);
+            if (form_data[key] != '') {
+                if (!isNaN(x) && !form_data[key].includes("-")) {
+                    query += key + " = " + form_data[key] + ", ";
+                } else {
+                    query += key + " = '" + form_data[key] + "', ";
+                }
+            }
+        }
+    }
+
+    query = query.slice(0, -2);
+    query += " WHERE " + id_key + " = " + id_value + ";";
+
+    console.log(query);
+    return new Promise (() => {
+        db.all(query, (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    });
+}
+
+function dbInsert(table, form_data) {
+    let query = "INSERT INTO " + table + " VALUES(";
+    for (const key in form_data) {
+        let x = parseFloat(form_data[key]);
+        if (!isNaN(x) && !form_data[key].includes("-")) {
+            query += form_data[key] + ", ";
+        } else {
+            query += "'" + form_data[key] + "', ";
+        }
+    }
+    query = query.slice(0, -2) + ");";
+    console.log(query);
+    
+    return new Promise (() => {
+        db.all(query, (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    });
+}
+
+function dbDelete(table, form_data) {
+    let query = "DELETE FROM " + table;
+    let flag = false;
+    for (const key in form_data) {
+        if (form_data[key] != '') {
+            query += " WHERE ";
+            flag = true;
+            break;
+        }
+    }
+
+    if (flag) {
+        for (const key in form_data) {
+            let x = parseFloat(form_data[key]);
+            if (form_data[key] != '') {
+                if (!isNaN(x) && !form_data[key].includes("-")) {
+                    query += key + " = " + form_data[key] + " AND ";
+                } else {
+                    query += key + " = '" + form_data[key] + "' AND ";
+                }
+            }
+        }
+        query = query.slice(0, -5);
+    }
+    query += ";";
+    console.log(query);
+
+    return new Promise(() => {
+        db.all(query, (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    });
+}
+
 app.get('', (req, res) => {
     res.sendFile(path.join(__dirname, 'WayPointCreation.html'));
+    
 });
 
 app.post('/way', (req, res) => {
-    let data = [];
-    data.push(req.body["WaypointID"]);
-    data.push("'"+req.body["Title"]+"'");
-    data.push(req.body["Longitude"]);
-    data.push(req.body["Latitude"])
-    data.push(req.body["Northing"]);
-    data.push(req.body["Easting"]);
-    data.push(req.body["UTMZone1"]);
-    data.push("'"+req.body["UTMZone2"]+"'");
-    data.push("'"+req.body["Datum"]+"'");
-    data.push("'"+req.body["Projection"]+"'");
-    data.push("'"+req.body["FieldBook"]+"'");
-    data.push("'"+req.body["FieldbookPage"]+"'");
-    data.push("'"+req.body["Formation"]+"'");
-    data.push("'"+req.body["SiteName"]+"'");
-    data.push("'"+req.body["DateCollection"]+"'");
-    data.push(req.body["Elevation"]);
-    data.push("'"+req.body["ProjectName"]+"'");
-    data.push(req.body["MeasuredSection"]);
-    data.push("'"+req.body["SectionName"]+"'");
-    data.push("'"+req.body["Comments"]+"'");
-    let query = "INSERT INTO Waypoint VALUES(" +data[0];
-    for (let i = 1; i < data.length; i++) {
-        query += ", " + data[i];
+    let operation = req.body["operation"];
+    let table = "Waypoint"
+    delete req.body.operation;
+    if (operation == "insert") {
+        dbInsert(table, req.body);
+    } else if (operation == "delete") {
+        dbDelete(table, req.body);
+    } else if (operation == "update") {
+        dbUpdate(table, req.body);
+    } else if (operation == "select") {
+        let promise = dbSelect(table, req.body);
+        promise.then((results) => {
+            console.log(results);
+        })
     }
-    query += ");";
-    console.log(query)
-    db.all(query);
-    
+
     res.send("nice job!");
 });
 
@@ -57,7 +171,23 @@ app.post('/meso', (req, res) => {});
 app.post('/thin', (req, res) => {});
 app.post('/photo', (req, res) => {});
 app.post('/employee', (req, res) => {
-    console.log(req.body);
+    let operation = req.body["operation"];
+    let table = "Employee"
+    delete req.body.operation;
+    if (operation == "insert") {
+        dbInsert(table, req.body);
+    } else if (operation == "delete") {
+        dbDelete(table, req.body);
+    } else if (operation == "update") {
+        dbUpdate(table, req.body);
+    } else if (operation == "select") {
+        let promise = dbSelect(table, req.body);
+        promise.then((results) => {
+            console.log(results);
+        })
+    }
+
+    res.send("nice job!");
 });
 app.post('/analyze', (req, res) => {});
 app.post('/problem', (req, res) => {});
